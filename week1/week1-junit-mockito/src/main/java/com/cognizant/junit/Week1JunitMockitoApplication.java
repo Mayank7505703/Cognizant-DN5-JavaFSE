8900773b12307c8cbb8622243aee57329bfd70d7
Mayank7505703 <karansharma90900@gmail.com>
2026-06-24 14:43:07 +0530

    Added Week 1 JUnit Mockito exercises

---
+++ week1/week1-junit-mockito/src/main/java/com/cognizant/junit/Week1JunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1JunitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week1JunitMockitoApplication.class, args);
+	}
+
+}
---
+++ week1/week1-junit-mockito/src/test/java/com/cognizant/junit/Week1JunitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week1JunitMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
